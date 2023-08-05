a88343d6f5ff08e12c84d7809fc5f240f62b5545
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:38:14 +0000

    Backup Commit

---
+++ springapp/src/test/java/com/examly/springapp/glue.java
@@ -0,0 +1,19 @@
+package com.examly.springapp;
+
+public class glue {
+    
+}
+
+
+
+
+
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources", glue = "stepdefinitions")
+public class TestRunner {
+}
